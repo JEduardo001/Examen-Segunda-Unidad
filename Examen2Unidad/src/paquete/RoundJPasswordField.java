63698be83dfd2c6dc6ff2ae88a175ddd770336ab
JEduardo001 <125694974+JEduardo001@users.noreply.github.com>
2023-03-26 16:01:51 -0600

    Add files via upload

---
+++ Examen2Unidad/src/paquete/RoundJPasswordField.java
@@ -0,0 +1,51 @@
+package paquete;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JPasswordField;
+
+public class RoundJPasswordField extends JPasswordField{
+	 private Color backgroundColor;
+	    private Color foregroundColor;
+	    private int borderRadius;
+
+	    public RoundJPasswordField() {
+	        super();
+	        this.backgroundColor = Color.WHITE;
+	        this.foregroundColor = Color.BLACK;
+	        this.borderRadius = 30;
+	        setOpaque(false);
+	    }
+
+	    @Override
+	    protected void paintComponent(Graphics g) {
+	        Graphics2D g2 = (Graphics2D) g.create();
+	        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	        g2.setColor(backgroundColor);
+	        g2.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, borderRadius, borderRadius);
+	        g2.setColor(foregroundColor);
+	        super.paintComponent(g2);
+	        g2.dispose();
+	    }
+
+	    @Override
+	    public Dimension getPreferredSize() {
+	        return new Dimension(super.getPreferredSize().width + borderRadius*2, super.getPreferredSize().height + borderRadius*2);
+	    }
+
+	    public void setBackgroundColor(Color backgroundColor) {
+	        this.backgroundColor = backgroundColor;
+	    }
+
+	    public void setForegroundColor(Color foregroundColor) {
+	        this.foregroundColor = foregroundColor;
+	    }
+
+	    public void setBorderRadius(int borderRadius) {
+	        this.borderRadius = borderRadius;
+	    }
+}
